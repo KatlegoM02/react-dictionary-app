076ef1fae6e79bc6430efb1651affb80ede9f2d6
KatlegoM <katlegomakgatle@gmail.com>
2024-07-18 23:06:00 +0200

    added a footer of my project details

--- src/App.js
+++ src/App.js
@@ -13,8 +13,36 @@ function App() {
   return (
     <div className="container">
       <h1 className="header">Dictionary App</h1>
-      <DictionaryForm onDefinitions={handleDefinitions} />
-      <DefinitionList definitions={definitions} />
+      <div className="box">
+        <DictionaryForm onDefinitions={handleDefinitions} />
+        <DefinitionList definitions={definitions} />
+      </div>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://github.com/KatlegoM02"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Katlego Makgatle
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/KatlegoM02/react-dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://react-dictionary-app.onrender.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Render
+        </a>
+      </footer>
     </div>
   );
 }
